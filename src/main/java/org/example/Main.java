5913bfbd5fa80d05b117bb9bebeddcb717291161
Vini-cods <vinisanto141@gmail.com>
2025-07-26 12:36:01 -0300

    Add initial implementation of Main class and VCS configuration

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,15 @@
 package org.example;
 
+import java.util.ArrayList;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
-        
+        ArrayList<String> nomes = new ArrayList<>();
+        nomes.add("Alice");
+        nomes.add("Bob");
 
     }
 }
